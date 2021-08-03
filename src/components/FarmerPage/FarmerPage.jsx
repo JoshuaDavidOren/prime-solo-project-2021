0183f2532e8bf7f577c936e2fc7a38e3419d2d64
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-03 10:03:59 -0500

    created turnary for user type

--- src/components/FarmerPage/FarmerPage.jsx
+++ src/components/FarmerPage/FarmerPage.jsx
@@ -6,11 +6,12 @@ function FarmerPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
+      
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <h1>YOUR A FARMER</h1>
       <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <p>Your user type is: {user.user_type? "true" : "false"}</p>
     </div>
   );
 }
