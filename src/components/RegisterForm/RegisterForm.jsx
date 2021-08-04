316f6aee57420f3886127c8cb91201852648e50f
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-03 20:58:27 -0500

    created regester user imputs realated to creating user profile

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -8,6 +8,12 @@ function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [userType, setUserType] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [farmName, setFarmName] = useState('');
+
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
@@ -58,17 +64,79 @@ function RegisterForm() {
               onChange={(event) => setPassword(event.target.value)}
             />
           </label>
-          <div></div>
-          <label htmlFor="password">
-          Check for Farmer:
+          </div>
+          <div>
+          <label htmlFor="fistName">
             <input
-              type="checkbox"
-              name="type"
-              value={!userType}
-              onChange={(event) => setUserType(event.target.value)}
+              className="inputStyle-1"
+              placeholder="First Name"
+              type="text"
+              value={firstName}
+              required
+              onChange={(event) => setFirstName(event.target.value)}
             />
           </label>
-        </div>
+          </div>
+          <div>
+          <label htmlFor="lastName">
+            <input
+              className="inputStyle-1"
+              placeholder="Last Name"
+              type="text"
+              value={lastName}
+              required
+              onChange={(event) => setLastName(event.target.value)}
+            />
+          </label>
+          </div>
+          <div>
+          <label htmlFor="phoneNumber">
+            <input
+            className="inputStyle-1"
+            placeholder="Phone Number"
+              type="text"
+              value={phoneNumber}
+              required
+              onChange={(event) => setPhoneNumber(event.target.value)}
+            />
+          </label>
+          </div>
+          <div>
+          <label htmlFor="email">
+            <input
+            className="inputStyle-1"
+            placeholder="@email"
+              type="text"
+              value={email}
+              required
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </label>
+          </div>
+          <div></div>
+          <div>
+            Check for Farmer:
+              <input
+                type="checkbox"
+                name="type"
+                value={!userType}
+                onChange={(event) => setUserType(event.target.value)}
+              />
+          </div>
+          {userType &&  
+          <div>
+          <label htmlFor="farName">
+            <input
+            className="inputStyle-1"
+            placeholder="Farm Name"
+              type="text"
+              value={farmName}
+              required
+              onChange={(event) => setFarmName(event.target.value)}
+            />
+          </label>
+          </div> 
+          }
         <div>
           <input className="btn" type="submit" name="submit" value="Register" />
         </div>
@@ -76,5 +144,7 @@ function RegisterForm() {
     </center>
   );
 }
+
+
 
 export default RegisterForm;
