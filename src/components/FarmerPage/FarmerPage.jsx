c6ef126d748bf669f5506415842bf5bf0ce6dca4
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-06 11:41:12 -0500

    added padding to farmerpage

--- src/components/FarmerPage/FarmerPage.jsx
+++ src/components/FarmerPage/FarmerPage.jsx
@@ -15,38 +15,41 @@ function FarmerPage() {
 
   console.log(user);
   return (
-    <section>
-      <header>
-        <div className="profile-image"></div>
-        <h3>{info.page_title}</h3>
-        <h4>{info.email}</h4>
-        <h4>{info.phone_number}</h4>
-        <p>
-          discription of farmer with beautiful words and thinsg obut the farm
-          that has been in there family for one million generations thank you
-          farmer and buyres THANK YOU
-        </p>
-      </header>
-        <section>
-          <h2>Groceries For Sale</h2>
-          {products.map((item) => {
-            return (
-              <ItemList
-                id={item.id}
-                available={item.available}
-                title={item.item}
-                price={item.asking_price}
-                product_id={item.product_id}
-                quantity={item.quantity}
-              />
-            );
-          })}
-        </section> 
-
-      <div>
-         
-      </div>
-    </section>
+    
+      <section>
+        <header class="p-head">
+          <div className="profile-image"></div>
+          <h3>{info.page_title}</h3>
+          <h4>{info.email}</h4>
+          <h4>{info.phone_number}</h4>
+          <p>
+            discription of farmer with beautiful words and thinsg obut the farm
+            that has been in there family for one million generations thank you
+            farmer and buyres THANK YOU
+          </p>
+        </header>
+        <center>
+          <section>
+            <h2>Groceries For Sale</h2>
+            {products.map((item) => {
+              return (
+                <ItemList
+                  id={item.id}
+                  available={item.available}
+                  title={item.item}
+                  price={item.asking_price}
+                  product_id={item.product_id}
+                  quantity={item.quantity}
+                />
+              );
+            })}
+          </section>
+        <div>
+      
+        </div>
+        </center>
+      </section>
+    
   );
 }
 
