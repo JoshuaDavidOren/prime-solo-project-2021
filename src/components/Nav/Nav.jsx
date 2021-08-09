8693b88b08276bc1c79059b1d5fdf28fc1006c0b
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-09 15:24:25 -0500

    dope dropdown for the menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -16,13 +16,71 @@ import Zoom from '@material-ui/core/Zoom';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import clsx from "clsx";
+import Drawer from "@material-ui/core/Drawer";
+
+import List from "@material-ui/core/List";
+
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     position: 'fixed',
     bottom: theme.spacing(2),
     right: theme.spacing(2),
   },
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
+  }
 }));
+
+
+export default function TemporaryDrawer(props) {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom"
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      
+    </div>
+  );
 
 function ScrollTop(props) {
   const { children, window } = props;
@@ -62,33 +120,47 @@ ScrollTop.propTypes = {
   window: PropTypes.func,
 };
 
-export default function BackToTop(props) {
+
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <AppBar style={{ backgroundColor: '#bec9bc', color: '#132411' }}>
-        <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="open drawer">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">Scroll to see button</Typography>
-        </Toolbar>
-      </AppBar>
-      <Toolbar id="back-to-top-anchor" />
-      <Container>
-        <Box my={2}>
-          
-        </Box>
-      </Container>
-      <ScrollTop {...props}>
-        <Fab color="secondary" size="small" aria-label="scroll back to top"
-        style={{backgroundColor: '#497442', color: '#FFFFFF'}}>
-          <KeyboardArrowUpIcon />
-        </Fab>
-      </ScrollTop>
-    </React.Fragment>
-  );
+
+    <div>
+      {["top"].map((anchor) => (
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar style={{ backgroundColor: '#bec9bc', color: '#132411' }}>
+          <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="open drawer"onClick={toggleDrawer(anchor, true)}>
+              <MenuIcon />
+            </IconButton >
+            <Drawer
+              anchor={anchor}
+              open={state[anchor]}
+              onClose={toggleDrawer(anchor, false)}
+            >
+              {list(anchor)}
+            </Drawer>
+            <Typography variant="h6">Scroll to see button</Typography>
+          </Toolbar>
+        </AppBar>
+        <Toolbar id="back-to-top-anchor" />
+        <Container>
+          <Box my={2}>
+      
+          </Box>
+        </Container>
+        <ScrollTop {...props}>
+          <Fab color="secondary" size="small" aria-label="scroll back to top"
+          style={{backgroundColor: '#497442', color: '#FFFFFF'}}>
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollTop>
+      </React.Fragment>
+    
+    ))
+    
 }
+</div>
+  );}
 
 
 // function Nav() {
